2c53606dc0fa66fb091574c5e4b63df828b00c29
Victor Ordu <victorordu@outlook.com>
2019-11-17 09:16:29 +0100

    New tests added

---
+++ tests/testthat/test-extraction.R
@@ -0,0 +1,19 @@
+library(raampGBV)
+
+test_that("'extract_vector()` validates input", {
+  expect_type(v, "character")
+  expect_is(v, "character")
+  expect_error(extract_vector())
+  expect_error(extract_vector(999))
+  expect_error(extract_vector(TRUE))
+  expect_error(extract_vector(NA))
+  expect_error(extract_vector(NA_character_))
+})
+
+test_that("extracted vector has no spaces", {
+  str <- quote("c(\"no.space\", \"has. space\")")
+  v <- extract_vector(str)
+
+  expect_false(any(grepl("\\s+", v)))
+  expect_length(v, 2L)
+})
