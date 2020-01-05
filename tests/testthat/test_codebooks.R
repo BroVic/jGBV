c9bced2860c97351380b88d5a3b3b263d0ad5507
Victor Ordu <victorordu@outlook.com>
2020-01-05 19:46:07 +0100

    A few more unit tests

--- tests/testthat/test_codebooks.R
+++ tests/testthat/test_codebooks.R
@@ -1,5 +1,10 @@
 library(raampGBV)
 
 test_that("input can be validated", {
+  testOutput <- .getCodebookFileName("Abia", "Health")
 
+  expect_error(.getCodebookFileName("Borno", "Health"))
+  expect_error(.getCodebookFileName("Abia", "NotSector"))
+  expect_identical(testOutput, "codebook_Abia_Health.html")
+  expect_false(identical(testOutput, "codebook_Health_Abia.html"))
 })
