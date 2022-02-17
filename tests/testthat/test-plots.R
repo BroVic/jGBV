66294426c38eb907a2a283b337ab5e9f7022b223
Victor Ordu <victorordu@outlook.com>
2022-02-17 18:41:18 +0100

    Review tests

--- tests/testthat/test-plots.R
+++ tests/testthat/test-plots.R
@@ -1,9 +1,13 @@
 
 test_that("plots for the report are made", {
-  options(data.on.redcap = FALSE)
+  op <- options(data.on.redcap = FALSE, use.regex = FALSE)
+
   file <-"testdata/dft-out.rds"
-  try(data <- readRDS(file))
+  data <- readRDS(file)
+
   x <- show_output(data, index = 2:4)
 
   expect_s3_class(x, "ggplot")
+
+  options(op)
 })
--- tests/testthat/test_objects.R
+++
@@ -1,6 +0,0 @@
-test_that("Objects have been properly created", {
-  expect_identical(raampStates, c("Abia", "Akwa Ibom", "Ogun", "Bauchi"))
-  expect_type(tool.sectors, "character")
-  expect_is(tool.sectors, "Tools")
-  expect_is(print(tool.sectors), "character")
-})
